68a2016b4bc9ccb0b63523e4ea5de816613f5840
LuizHenriqueSena <coelho.luiz.sena@gmail.com>
2019-02-09 18:36:34 -0400

    adicionado solvers e benchmarks

--- ssvlab/verifynn/verifynn.h
+++ ssvlab/verifynn/verifynn.h
@@ -430,25 +430,29 @@ float lookup[4000] = {0.000000 ,0.000000 ,0.000000 ,0.000000 ,0.000000 ,0.000000
 
 float sqrtt(float num)
 {
+	int i = 0;
     float guess, e, upperbound;
     guess = 1;
     e = 0.001;
     do
     {
+				i++;
         upperbound = num / guess;
         guess = (upperbound + guess) / 2;
     } while (!(guess * guess >= num - e &&
                guess * guess <= num + e));
-    return guess;
+							 printf("VALOR DE %d \n", i);
+		return guess;
 }
 
-int isCloseEnough(float* img, float* adversarial, float b, int size) {
+int isCloseEnough(float* img, float* img2, float b, int size) {
 		int i = 0;
 		float normDistance = 0;
 		for(i=0;i<size;i++) {
-			normDistance += (img[i]-adversarial[i])*(img[i]-adversarial[i]);
+			normDistance += (img[i] - img2[i])*(img[i] - img2[i]);
 		}
-			normDistance = sqrtt(normDistance);
+
+		normDistance = sqrtt(normDistance);
 			if(normDistance <= b)
 				return 1;
 			else
@@ -568,14 +572,14 @@ int cont = 0;
         //printf("limiar de ativacao da posicao: %d com valor: %.2f \n", cont, pesosSinapticos[xn]);
 }
 
-void checkNNLUT(float* wfc1, float* bfc1, float* wfc2, float* bfc2, float* wfc3, float* bfc3, float* img) {
+void checkNNLUT(float wfc1[125], float bfc1[5], float wfc2[20], float bfc2[4], float wfc3[20], float bfc3[5], float img[25]) {
 
-	float *x1layer1;
-	float *x1layer2;
-	float *x1layer3;
-	float *x2layer1;
-	float *x2layer2;
-	float *x2layer3;
+//	float *x1layer1;
+//	float *x1layer2;
+//	float *x1layer3;
+//	float *x2layer1;
+//	float *x2layer2;
+//	float *x2layer3;
 	float alpha;
 	float beta;
 	//float* dev_result;
@@ -593,18 +597,24 @@ void checkNNLUT(float* wfc1, float* bfc1, float* wfc2, float* bfc2, float* wfc3,
 	//wfc1 = (float*)malloc(data*fc1*sizeof(float));
 
 
-	x1layer1 = (float*)malloc(fc1*sizeof(float));
+	//x1layer1 = (float*)malloc(fc1*sizeof(float));
 
-	x1layer2 = (float*)malloc(fc2*sizeof(float));
+	//x1layer2 = (float*)malloc(fc2*sizeof(float));
 
-	x1layer3 = (float*)malloc(fc3*sizeof(float));
+	//x1layer3 = (float*)malloc(fc3*sizeof(float));
 
-	x2layer1 = (float*)malloc(fc1*sizeof(float));
+	//x2layer1 = (float*)malloc(fc1*sizeof(float));
 
-	x2layer2 = (float*)malloc(fc2*sizeof(float));
+	//x2layer2 = (float*)malloc(fc2*sizeof(float));
 
-	x2layer3 = (float*)malloc(fc3*sizeof(float));
+	//x2layer3 = (float*)malloc(fc3*sizeof(float));
 
+
+		float x1layer1[5] = {0,0,0,0,0};
+
+		float x1layer2[4]= {0,0,0,0} ;
+
+		float x1layer3[5]= {0,0,0,0,0};
 
 
   normalizef(img, 25); // ponteiro da entrada e tamanho da imagem
@@ -678,9 +688,9 @@ void checkNNLUT(float* wfc1, float* bfc1, float* wfc2, float* bfc2, float* wfc3,
 	//imprimeResultante(x1layer3, fc3);
 	activeSigmoidLUT(x1layer3, fc3);
 	imprimeResultante(x1layer3, fc3);
-
+//	float a[3] = {0.8, 0.8, 0.8};
 	//Computing the third layer of the second image on the same Neural network
-
+	__ESBMC_assert(x1layer3[4] < 0.5, "Image is not a U");
 
 
 }

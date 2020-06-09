d4a2c97c50695e8bb9fd91ddf9675c48708c3acd
LuizHenriqueSena <coelho.luiz.sena@gmail.com>
2020-06-09 11:58:31 -0400

    update

--- ssvlab/simbolicChecking/main.c
+++ ssvlab/simbolicChecking/main.c
@@ -2,20 +2,22 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-//esbmc-gpu verifynn.c --force-malloc-success --no-bounds-check --no-div-by-zero-check --no-pointer-check --incremental-bmc --interval-analysis
+//esbmc-gpu main.c --force-malloc-success --no-bounds-check --no-div-by-zero-check --no-pointer-check --incremental-bmc --interval-analysis
 int main(){
   clock_t t;
   t = clock();
   float y = nondet_float();
+  __ESBMC_assume(y >= 0 && y <= 1);
   float x = nondet_float();
-  safeLimit = 5.5;
+  __ESBMC_assume(x >= 0 && x <= 1);
+  float safeLimit = 3.6;
   // Restrictions
   //   -3.000000  2.000000  0.000000
   //   4.000000  1.000000  0.000000
   // Simbolic Propagation:
   //   1.000000  3.000000  0.000000
-  __ESBMC_assert(-3*x + 2*y > 0);
-  __ESBMC_assert(4*x + 1*y > 0);
+  __ESBMC_assume(-3*x + 2*y > 0);
+  __ESBMC_assume(4*x + 1*y > 0);
 
-  __ESBMC_assume(x + 3*y - safeLimit > 0, "Unsafe");
+  __ESBMC_assert(x + 3*y - safeLimit < 0, "Unsafe");
 }

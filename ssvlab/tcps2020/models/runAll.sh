a7511c6609f8960b9af791f3ca7cc6e490cbe9f8
root <root@fm07.cs.manchester.ac.uk>
2020-08-12 22:23:52 +0100

    script corrections

--- ssvlab/tcps2020/models/runAll.sh
+++ ssvlab/tcps2020/models/runAll.sh
@@ -2,9 +2,13 @@ dir=$(pwd)
 if [ $1 == "esbmc" ]; then
 	echo "ESBMC"
 	flags="--fixedbv --force-malloc-success --no-bounds-check --no-div-by-zero-check --no-pointer-check --incremental-bmc --memlimit 85g"
+fi
 echo $flags
 
 for d in $(find $(dir) -type f -name "*.c")
 do
-	echo $d
+	command="~/esbmc/v6.4.0/bin/esbmc"
+	command="$command $d $flags"
+	echo "$command"
 done
+

e54dbc0e31c0bd280bf22783b054e154c49387c8
Luiz Sena <coelho.luiz.sena@gmail.com>
2020-08-12 18:03:06 -0400

    model corrections

--- ssvlab/tcps2020/models/runAll.sh
+++ ssvlab/tcps2020/models/runAll.sh
@@ -3,12 +3,12 @@ if [ $1 == "esbmc" ]; then
 	echo "ESBMC"
 	flags="--fixedbv --force-malloc-success --no-bounds-check --no-div-by-zero-check --no-pointer-check --incremental-bmc --memlimit 85g"
 fi
-echo $flags
+
 
 for d in $(find $(dir) -type f -name "*.c")
 do
+	name=$(echo "$d" | cut -f 1 -d '.')
 	command="~/esbmc/v6.4.0/bin/esbmc"
-	command="$command $d $flags"
+	command="$command $d $flags ${name}_LOG.txt"
 	echo "$command"
 done
-

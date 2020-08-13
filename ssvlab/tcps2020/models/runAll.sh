02514b78b8a1d0980b0378a9423821f5def3c5a2
root <root@fm07.cs.manchester.ac.uk>
2020-08-13 04:11:36 +0100

    logs

--- ssvlab/tcps2020/models/runAll.sh
+++ ssvlab/tcps2020/models/runAll.sh
@@ -4,11 +4,11 @@ if [ $1 == "esbmc" ]; then
 	flags="--fixedbv --force-malloc-success --no-bounds-check --no-div-by-zero-check --no-pointer-check --incremental-bmc --memlimit 85g"
 fi
 
-
 for d in $(find $(dir) -type f -name "*.c")
 do
 	name=$(echo "$d" | cut -f 1 -d '.')
-	command="~/esbmc/v6.4.0/bin/esbmc"
-	command="$command $d $flags > ${name}_LOG.txt 2>&1"
-	$command
+	command="/home/erickson.alves/esbmc/v6.4.0/bin/esbmc"
+	command="$command $d $flags"
+	echo "$command"
+	$command | tee "$dir"/"$name"_LOG.txt
 done

839aafc17e7b71349c674891e4d454b5860511cf
root <root@fm07.cs.manchester.ac.uk>
2020-08-14 11:16:26 +0100

    results

---
+++ ssvlab/tcps2020/models/covering/runAll.sh
@@ -0,0 +1,14 @@
+dir=$(pwd)
+if [ $1 == "esbmc" ]; then
+	echo "ESBMC"
+	flags="-I . --fixedbv --force-malloc-success --no-bounds-check --no-div-by-zero-check --no-pointer-check --incremental-bmc --memlimit 85g"
+fi
+
+for d in $(find $(dir) -type f -name "*.c")
+do
+	name=$(echo "$d" | cut -f 1 -d '.')
+	command="/home/erickson.alves/esbmc/v6.4.0/bin/esbmc"
+	command="$command $d $flags"
+	echo "$command"
+	$command | tee "$dir"/"$name"_LOG.txt
+done
